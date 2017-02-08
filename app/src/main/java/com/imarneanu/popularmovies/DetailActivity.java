8ca1d5617925a3f375964b65c5a85b07f28b861c
imarneanu <iulia.marneanu@p3-group.com>
2017-02-08 15:34:28 +0200

    Movie details first draft.

--- app/src/main/java/com/imarneanu/popularmovies/DetailActivity.java
+++ app/src/main/java/com/imarneanu/popularmovies/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.imarneanu.popularmovies;
 
 import com.imarneanu.popularmovies.data.Movie;
+import com.imarneanu.popularmovies.data.NetworkUtils;
+import com.squareup.picasso.Picasso;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by imarneanu on 2/8/17.
@@ -17,11 +20,25 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        setTitle(R.string.activity_detail);
 
-        if (getIntent().getParcelableExtra(EXTRA_MOVIE) != null) {
-            Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
-            Log.d(EXTRA_MOVIE, movie.getOriginalTitle());
+        Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
+        if (movie == null) {
+            return;
         }
+        setData(movie);
+    }
 
+    private void setData(Movie movie) {
+        TextView title = (TextView) findViewById(R.id.details_original_title);
+        title.setText(movie.getOriginalTitle());
+        ImageView poster = (ImageView) findViewById(R.id.details_poster);
+        Picasso.with(this).load(NetworkUtils.buildImageUrlPath(movie.getPosterPath())).into(poster);
+        TextView releaseDate = (TextView) findViewById(R.id.details_release_date);
+        releaseDate.setText(movie.getReleaseDate());
+        TextView userRating = (TextView) findViewById(R.id.details_user_rating);
+        userRating.setText(movie.getUserRating());
+        TextView plot = (TextView) findViewById(R.id.details_plot);
+        plot.setText(movie.getPlot());
     }
 }
