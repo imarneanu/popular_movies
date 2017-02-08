c6380a784419a15c5a371c98c18931cb5c5e4d46
imarneanu <iulia.marneanu@p3-group.com>
2017-02-08 11:37:24 +0200

    Implemented get response method from url.

--- app/src/main/java/com/imarneanu/popularmovies/data/NetworkUtils.java
+++ app/src/main/java/com/imarneanu/popularmovies/data/NetworkUtils.java
@@ -5,8 +5,12 @@ import com.imarneanu.popularmovies.BuildConfig;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Created by imarneanu on 2/7/17.
@@ -62,5 +66,22 @@ public class NetworkUtils {
 
     public static String buildImageUrlPath(String thumbnail) {
         return IMAGE_BASE_URL.concat(IMAGE_SIZE).concat(thumbnail);
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            }
+            return null;
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
