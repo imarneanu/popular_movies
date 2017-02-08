3fea50ed6bdfcc432cdddec9bbda22c1987261a3
imarneanu <iulia.marneanu@p3-group.com>
2017-02-08 11:38:05 +0200

    Added AsyncTask for making http calls.

--- app/src/main/java/com/imarneanu/popularmovies/MainActivity.java
+++ app/src/main/java/com/imarneanu/popularmovies/MainActivity.java
@@ -2,10 +2,13 @@ package com.imarneanu.popularmovies;
 
 import com.imarneanu.popularmovies.data.NetworkUtils;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "URL: " + url);
         url = NetworkUtils.buildUrl(NetworkUtils.UrlType.MOVIE_DETAILS, "328111");
         Log.d(TAG, "URL: " + url);
+
+        new MovieDbQueryTask().execute(NetworkUtils.buildUrl(NetworkUtils.UrlType.POPULAR));
+    }
+
+    private void loadPopularMovies(String s) {
+
+    }
+
+    public class MovieDbQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL url = urls[0];
+            String responseString = null;
+            try {
+                responseString = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (TextUtils.isEmpty(s)) {
+                return;
+            }
+            loadPopularMovies(s);
+        }
     }
 }
